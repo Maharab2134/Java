8e277fa3b2f65de241d9cb965903dbee1c72c7f2
Maharab2134 <maharabhosen365@gmail.com>
2023-11-20 17:54:10 +0600

    New

--- Code/Q1.java
+++ Code/Q1.java
@@ -1,4 +1,14 @@
 // write a program to print the sum of two numbers
-// Input: 10 20
-// Output: 30
-khdgbhbghb
+package Code;
+
+/**
+ * Q1
+ */
+public class Q1 {
+
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("Sum of " + a + " and " + b + " is " + (a + b));
+    }
+    
+}
